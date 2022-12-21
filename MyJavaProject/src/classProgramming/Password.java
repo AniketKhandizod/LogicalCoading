a5f6e9f9129df0de26f976a9877f4c62ee849d26
gtxtrex12323@gmail.com <gtxtrex12323@gmail.com>
2022-12-21 20:35:37 +0530

    Yo

--- MyJavaProject/src/classProgramming/Password.java
+++ MyJavaProject/src/classProgramming/Password.java
@@ -1,31 +1,64 @@
 package classProgramming;
-import java.util.Scanner;  // i imported utlity scanner class
-public class Password {
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
-	public static void main(String[] args) {
+import org.apache.commons.lang3.RandomStringUtils;  
+public class Password {
+	static Scanner SC;
+	String Token; 
+	public  Object[] CreateAccount() {
+	    SC = new Scanner (System.in); 
+		System.out.println("To Create Account Give ID = ");
+		String ID = SC.nextLine();
+		System.out.println("To Create Account Give ID = ");
+		String Pass = SC.nextLine();
 		
-		// F = 555;
-		//int D = 555;
+		return new Object[] {ID,Pass}; 
+	}
+	public String Login() {
+		Object[] A = new Password().CreateAccount();				
+		System.out.println("What is your User_name : ");    
+		String User_name = SC.next().trim();	 		
+		System.out.println("What is your Password : ");  
+		String Password = SC.next().trim();                
 		
-		Scanner SC = new Scanner (System.in);    // i created object here
-		
-		System.out.println("What is your User_name : ");    // i written printing statement to notify user about input
-		String User_name = SC.next().trim();	                    // 	i created here scanner method to get username
-		
-		System.out.println("What is your Password : ");    // i written printing statement to notify user about input
-		String Password = SC.next().trim();                        // 	i created here scanner method to get password
-		
-		
-		
-		if ((User_name.equals("XYZ")) && (Password.equals("XYZ"))) {
+		if ((User_name.equals(A[0])) && (Password.equals(A[1]))) {
 			System.out.println("You login succesfully");
-		}
-		
+			return RandomStringUtils.randomAlphanumeric(100);
+		}		
 		else {
 			System.out.println("Fail to login");
 		}
+		return null;
+	}
+	public void Verification() {
+		Token=new Password().Login();
+		String K=Token==(null)?("Fail"):(Token);
+		System.out.println(K.replaceAll("[0-9]", ""));
+		String Y = K.replaceAll("[^0-9]", "");
+		long C = Long.parseLong(Y);
+		System.out.println(C);
+	}
+	public void OpenFile() throws IOException, InterruptedException {
+		String [] path = {"rundll32 url.dll,FileProtocolHandler ","C:\\Users\\anike\\Desktop\\Laugh.png"};
+		File file = new File(path[1]);
+		if(file.exists()) {
+	        Process pro = Runtime.getRuntime().exec(path[0] + path[1]);
+	        pro.waitFor(5,TimeUnit.SECONDS);
+	        pro.exitValue();
+		}else {
+			System.out.println("File Not Found");
+		}
+		Desktop.getDesktop().open(file);
+	}
+	public static void main(String[] args) throws IOException, InterruptedException {
+		new Password().OpenFile();
+		
+	
+		
 		
 	}
 }
-//value we can compare
-//String is object type 
